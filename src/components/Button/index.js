38355122e5c296785ce73c060563b14a17543f3e
allrox <alexandre@allrox.com.br>
2024-10-28 23:00:24 -0300

    Pequenos ajustes de formatação

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,7 +2,7 @@ const Button = (props) => {
 
     const {icon, children} = props;
     return (
-        <button className="rounded-lg w-full bg-blue-800 hover:bg-blue-700 text-white px-3 py-3 md:px-7 md:py-5 mt-5 text-xl md:text-2xl">
+        <button className="rounded-lg w-full bg-blue-800 hover:bg-blue-700 text-white px-3 py-3 md:px-7 md:py-5 mt-5 text-xl md:text-2xl font-regular">
         {icon && <i className={`bi ${icon} me-2`}></i>}
         {children}</button>
     )
--- src/components/CardTitle/index.js
+++ src/components/CardTitle/index.js
@@ -1,6 +1,6 @@
 const CardTitle = (props) =>{
     return(
-        <h2 icon="" className="text-xl font-bold text-blue-800 line-clamp-1">{props.children}</h2>
+        <h2 icon="" className="text-xl font-bold text-blue-800 mb-2 line-clamp-1">{props.children}</h2>
     )
 }
 
